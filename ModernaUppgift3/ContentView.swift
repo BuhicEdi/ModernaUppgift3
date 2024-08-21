a175930174355a20ada2dbeec9d78926bd931f2c
Edi Buhic <edi@buhic.se>
2024-08-21 10:50:37 +0200

    Initial Commit

---
+++ ModernaUppgift3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ModernaUppgift3
+//
+//  Created by Edi Buhic on 2024-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModernaUppgift3/ModernaUppgift3App.swift
@@ -0,0 +1,17 @@
+//
+//  ModernaUppgift3App.swift
+//  ModernaUppgift3
+//
+//  Created by Edi Buhic on 2024-08-21.
+//
+
+import SwiftUI
+
+@main
+struct ModernaUppgift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
