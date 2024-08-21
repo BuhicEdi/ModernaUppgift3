efd53e777916a7d096b8854425d88adf6e2e3c9f
Edi Buhic <edi@buhic.se>
2024-08-21 14:00:15 +0200

    Moderna Swift Uppgift 3 (API)

--- ModernaUppgift3/ContentView.swift
+++ ModernaUppgift3/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var kanyeQuote: String = "..."
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Kanye West once said")
+                .bold()
+                .font(.system(size: 25))
+                .padding(30)
+            
+            Text(kanyeQuote)
+                .multilineTextAlignment(.center)
+
         }
-        .padding()
+        .onAppear(perform: getQuote)
+    }
+
+
+func getQuote() {
+    guard let apiURL = URL(string: "https://api.kanye.rest") else { return }
+    
+    URLSession.shared.dataTask(with: apiURL) { data, response, error in
+        if let data = data {
+            if let decodedResponse = try? JSONDecoder().decode(QuoteData.self, from: data) {
+                DispatchQueue.main.async {
+                    self.kanyeQuote = "\"\(decodedResponse.quote)\" "
+                    }
+                }
+            }
+        } .resume()
     }
 }
+    
+    struct QuoteData: Codable {
+        let quote: String
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
